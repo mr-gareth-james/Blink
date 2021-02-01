f44bb96cf0a2d3f6dae0f2aa48ba09627c2fbda4
mr-gareth-james <g23design@hotmail.com>
2021-02-01 13:17:45 +0000

    changed for mini leds

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,21 @@
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(LED_BUILTIN,OUTPUT); // comment
+  //pinMode(LED_BUILTIN,OUTPUT); // comment
+  pinMode(D2,OUTPUT); // comment
+  pinMode(D1,OUTPUT); // comment
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(LED_BUILTIN, true);
-  delay(50);
-  digitalWrite(LED_BUILTIN, false);
-  delay(50);
+  //digitalWrite(LED_BUILTIN, true);
+
+  digitalWrite(D2, true);
+  digitalWrite(D1, false);
+  delay(500);
+  //digitalWrite(LED_BUILTIN, false);
+
+  digitalWrite(D2, false);
+  digitalWrite(D1, true);
+  delay(500);
 }
